08dfe7888358307df8a6a7335140e45205f88437
George Edeh <gofy6566@gmail.com>
2024-09-20 16:57:07 +0100

    Worked on the email functionality

--- backend/nestfinder/settings.py
+++ backend/nestfinder/settings.py
@@ -151,3 +151,12 @@ REST_FRAMEWORK = {
     'TEST_REQUEST_DEFUALT_FORMAT': 'vnd.api+json'
 
 }
+
+#EMAIL CONFIGURATION
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
---
+++ backend/utils/send_mail.py
@@ -0,0 +1,65 @@
+import os
+from typing import Union
+from dotenv import load_dotenv
+from urllib.parse import urlparse
+load_dotenv()
+from django.core.exceptions import ValidationError
+from django.contrib.sessions.backends.db import SessionStore
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.core.mail import EmailMultiAlternatives
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+session = SessionStore()
+session_key = None
+
+class AccountActivationToken(PasswordResetTokenGenerator):
+    #modify it to generate tokens that expires after a given time
+    #probably hashes the current time and to unhash it the time has to be within a 
+    #specific range for the key to be passed
+    pass
+token_generator = AccountActivationToken()
+
+def get_parsed_url_from_request(uri):
+        return urlparse(uri)
+
+
+def send_email(request, user, **kwargs)-> Union[Response,None]:
+
+    
+    #Generate verification url for the email
+    verification_token = token_generator.make_token(user=user)
+    parsed_uri = get_parsed_url_from_request(request.build_absolute_uri())
+    url_scheme = parsed_uri.scheme
+    current_domain = parsed_uri.netloc
+    verification_link = f"{url_scheme}://{current_domain}/verify/?{verification_token}" 
+
+    """Logic for sending mail alos sends mail as plain text incase html cannot be rendered"""
+    html_message = render_to_string(f"{kwargs['mail_type']}.html",
+                                    {'username': user.first_name,
+                                    'link': verification_link})                      
+    plain_message = strip_tags(html_message) 
+    subject = "Password Reset" if kwargs['mail_type'] == 'password_reset' else "Verify your email"
+    from_email, to = os.environ.get('EMAIL_HOST_USER'), user.email
+    msg = EmailMultiAlternatives(subject, plain_message, from_email, [to])
+    msg.attach_alternative(html_message, "text/html")
+    try:
+        msg.send()
+    except ValidationError as e:
+        return Response(e.messages, status=status.HTTP_400_BAD_REQUEST)
+
+    
+
+
+def verify_token(user, token):
+    try:
+          user = token_generator.check_token(user, token)
+    except User.DoesNotExist as e:
+         return "Invlalid Link"
+    
+    return True
